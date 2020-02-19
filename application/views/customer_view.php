de7fe05e1ff09f51627c65eea8174200b5e7c0bc
ujitham <ujithamanuja@gmail.com>
2020-02-19 20:32:26 +0530

    cus reg

--- application/views/customer_view.php
+++ application/views/customer_view.php
@@ -5,12 +5,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Deptors Registration</h1>
+            <h1 class="m-0 text-dark">Debtors Registration</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">HRM module</a></li>
-              <li class="breadcrumb-item active">Deptors</li>
+              <li class="breadcrumb-item active">Debtors</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
@@ -19,7 +19,78 @@
     <!-- /.content-header -->
 
     <!-- Main content -->
+      <!-- Main content -->
     <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Quick Example</h3>
+              </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form class="form-horizontal">
+                <div class="card-body">
+                  <div class="form-group row">
+                    <label for="Debtors Name" class="col-sm-2 col-form-label">Debtors Name</label>
+                    <div class="col-sm-4">
+                      <input type="text" class="form-control" id="cusname" placeholder="Customer Name">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="Debtors Address" class="col-sm-2 col-form-label">Debtors Address</label>
+                    <div class="col-sm-4">
+                      <input type="text" class="form-control" id="cusadd" placeholder="Address">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="Contact No" class="col-sm-2 col-form-label">Contact No</label>
+                    <div class="col-sm-4">
+                      <input type="text" class="form-control" id="cuscontact" placeholder="Contact No">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="Credit Limit" class="col-sm-2 col-form-label">Credit Limit</label>
+                    <div class="col-sm-4">
+                      <input type="text" class="form-control" id="cuscreaditlim" placeholder="Credit limit">
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                  <label>Date range:</label>
+
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">
+                        <i class="far fa-calendar-alt"></i>
+                      </span>
+                    </div>
+                    <input type="text" class="form-control float-right" id="reservation">
+                  </div>
+                  <!-- /.input group -->
+                </div>
+
+                  
+                 
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-info">Sign in</button>
+                  <button type="reset" class="btn btn-default float-right">Cancel</button>
+                </div>
+                <!-- /.card-footer -->
+              </form>
+            </div>
+
+          </div>
+          <!--/.col (right) -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
   </div>
+
--- application/views/footer.php
+++ application/views/footer.php
@@ -18,9 +18,7 @@
 <!-- jQuery UI 1.11.4 -->
 <script src="<?php echo base_url('assets/plugins/jquery-ui/jquery-ui.min.js')?>"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
+
 <!-- Bootstrap 4 -->
 <script src="<?php echo base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
 <!-- ChartJS -->
@@ -44,8 +42,15 @@
 <!-- AdminLTE App -->
 <script src="<?php echo base_url('assets/dist/js/adminlte.js')?>"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?php echo base_url('assets/dist/js/pages/dashboard.js')?>"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="<?php echo base_url('assets/dist/js/demo.js')?>"></script>
+
+<!-- Page script -->
+<script>
+  $(function () {
+  //Date range picker
+    $('#reservation').daterangepicker()
+    })
+</script>
 </body>
 </html>
