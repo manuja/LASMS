a7a2ae1a29d4387dff04b98648d112685be67559
ujitham <ujithamanuja@gmail.com>
2020-02-22 20:56:52 +0530

    Uisr

--- application/views/stock/stockoutadd.php
+++ application/views/stock/stockoutadd.php
@@ -97,6 +97,7 @@
                     <button type="button" class="btn btn-primary" >View</button>
                     <button type="button" class="btn btn-success" >Edit</button>
                     <button type="button" class="btn btn-danger" >Delete</button>
+                    
                   </td>
 
                 </tr>
